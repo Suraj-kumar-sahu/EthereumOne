06db3623658bb2f4d9644ef8c02743d6667086ad
Surajkumarrr <surajsahu13sk@gmail.com>
2022-12-22 14:33:25 +0530

    src & public folder added to root project folder

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Search from './components/Search'
+import Domain from './components/Domain'
+
+// ABIs
+import EthereumOne from "./abis/EthereumOne.json" ;
+
+function App() {
+  const[account , setAccount] = useState(null) ;
+  const[provider, setProvider] = useState(null) ;
+  const[contract , setContract] = useState(null) ;
+  const[domains ,setDomains] = useState([]) ;
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" ;
+
+  useEffect(()=>{
+    const loadBlockchainData = async() =>{
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum) ;
+      setProvider(provider) ;
+
+      const contract = new ethers.Contract(contractAddress,EthereumOne,provider) ;
+      setContract(contract) ;
+
+      window.ethereum.on("accountsChanged",async()=>{
+        const accounts = await window.ethereum.request({ "method" : "eth_requestAccounts"}) ;
+        const account = ethers.utils.getAddress(accounts[0]) ;
+        setAccount(account) ;
+      })
+
+      const maxSupply = await contract.maxSupply() ;
+      console.log(maxSupply.toString());
+
+      const domains = [] ;
+      for(var i =1 ;i<= maxSupply ;i++){
+        const domain = await contract.getDomain(i) ;
+        domains.push(domain) ;
+      }
+      setDomains(domains) ;
+
+      const totalSupply = await contract.totalSupply() ;
+      console.log(totalSupply.toString());
+  
+    }
+    loadBlockchainData() ;
+  },[])
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount}/>
+      <Search/>
+      <div className='cards__section'>
+        <h2 className='cards__title'>Why you need a Domain name.</h2>
+        <p className='cards_description'>Own your custom username,use it across services and be able to store an avatar and other profile data.</p>
+
+        <hr />
+
+        <div className='cards'>
+          {
+          domains.map((domain,index) => (
+            <Domain domain={domain} id={index + 1} contract={contract} provider={provider} key={index}/>
+          ))
+          }
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Domain.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+const Domain = ({ domain, contract, provider, id }) => {
+  const[owner,setOwner] = useState(null) ;
+  const[hasSold,setHasSold] = useState(false) ;
+
+  const getOwner = async() =>{
+    if(domain.isOwned || hasSold){
+      const owner = await contract.ownerOf(id) ;
+      setOwner(owner) ;
+    }
+  }
+
+  const buyHandler = async() =>{
+    const signer = provider.getSigner() ;
+    const transaction = await contract.connect(signer).mint(id,{value: domain.cost}) ;
+    await transaction.wait() ;
+
+    setHasSold(true) ;
+  }
+
+  useEffect(()=>{
+    getOwner()
+  },[hasSold])
+
+  return (
+    <div className='card'>
+      <div className="card__info">
+        <h3>
+          { domain.isOwned || owner ? (
+            <del>{domain.name}</del>
+          ): (
+            <>{domain.name}</>
+          )}
+        </h3>
+
+        <>
+          <p>
+            {domain.isOwned || hasSold ? (
+              <>
+                <small>
+                  Owned by:<br/>
+                  <span>
+                    {owner && owner.slice(0,6)+"..."+owner.slice(38,42)} ;
+                  </span>
+                </small>
+              </>
+            ) : (
+              <h4>{ethers.utils.formatEther(domain.cost)} ETH</h4>
+            )}
+
+
+          </p>
+        </>
+      </div>
+
+      { !domain.isOwned && !owner &&
+        (<button type='button' className='card__button' onClick={buyHandler}>
+          Buy it
+      </button>)
+      }
+
+      
+    </div>
+  );
+}
+
+export default Domain;
